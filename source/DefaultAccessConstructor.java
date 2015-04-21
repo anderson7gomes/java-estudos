0c98e5c3253d25a17e2a383d14ae1fc5daa42613
anderson7gomes <gomes.andrade1@gmail.com>
2015-04-21 10:42:46 -0300

    DefaultAccessConstructor corrigido

---
+++ source/DefaultAccessConstructor.java
@@ -0,0 +1,14 @@
+/* 
+ * classe que declara uma classe pública mas com um 
+ * construtor com modificador de acesso padrão 
+ */
+ 
+package com.anderson.constructor;
+
+public class DefaultAccessConstructor {
+
+	DefaultAccessConstructor() {
+		System.out.println("Inside constructor DefaulAccessConstructor");
+	}
+
+} // fim da classe DefaulAccessConstructor 
