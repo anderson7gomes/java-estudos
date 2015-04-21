86a0db0cbe50f4af92999d9bca1498cb8113bb35
anderson7gomes <gomes.andrade1@gmail.com>
2015-04-21 10:40:42 -0300

    Demonstrando a tentativa de chamar um construtor com default access a partir de outro pacote

---
+++ source/DefaultAccessConstructorDemo.java
@@ -0,0 +1,13 @@
+/* classe que tenta utilizar o construtor da classe DefaultAccessConstructor */
+
+import com.anderson.constructor.DefaultAccessConstructor;
+
+class DefaultAccessConstructorDemo {
+
+	public static void main(String[] args) {
+	
+		DefaultAccessConstructor c = new DefaultAccessConstructor();
+	
+	}
+
+} // fim da classe DefaultAccessConstructorDemo
