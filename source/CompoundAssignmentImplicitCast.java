b41f1c93a89956cb6a076e25a42459b7be83c270
anderson7gomes <gomes.andrade1@gmail.com>
2015-04-26 10:09:04 -0300

    Demonstrando o cast implicito da atribuição composta

---
+++ source/CompoundAssignmentImplicitCast.java
@@ -0,0 +1,14 @@
+class CompoundAssignmentImplicitCast {
+
+	public static void main(String[] args) {
+	
+		int i = 1;
+		long l = Integer.MAX_VALUE;
+		
+		i += l;
+		
+		System.out.println(i);
+	
+	} 
+
+} // fim da classe CompoundAssignmentImplicitCast
