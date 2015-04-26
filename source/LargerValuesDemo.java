a9e72be7bd2abd2161d5e90a6ff4177330169132
anderson7gomes <gomes.andrade1@gmail.com>
2015-04-26 09:50:37 -0300

    Demonstrando o cast de valores maiores em variáveis menores

---
+++ source/LargerValuesDemo.java
@@ -0,0 +1,16 @@
+class LargerValuesDemo {
+
+	public static void main(String[] args) {
+	
+		byte b1 = 27;
+		byte b2 = 32;
+		byte result = (byte) (b1 + b2);
+		
+		long l = Integer.MAX_VALUE + 1;
+		int i = (int) l;
+		
+		System.out.println(i);
+	
+	}
+
+} // fim da classe LargerValuesDemo
