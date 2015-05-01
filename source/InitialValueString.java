a7e736706965f1c6cdeab26cf64a6d6a8c011b3e
anderson7gomes <gomes.andrade1@gmail.com>
2015-05-01 10:44:52 -0300

    Demonstrando o valor inicial de uma String quando criada com new

---
+++ source/InitialValueString.java
@@ -0,0 +1,12 @@
+class InitialValueString {
+
+	public static void main(String[] args) {
+	
+		String string = new String();
+		
+		System.out.printf("Initial value for a String is %s\n", 
+				(string.equals("")) ? "\"\"" : "unknown" );
+	
+	}
+
+} // fim da classe InititalValueString
