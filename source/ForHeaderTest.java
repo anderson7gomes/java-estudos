b1ecf926adae4d62c56cd169f331a04c3d68801f
anderson7gomes <gomes.andrade1@gmail.com>
2015-05-03 11:30:10 -0300

    Demonstrando que o laço for pode ter qualquer instrução na inicialização e na expressão de iteração

---
+++ source/ForHeaderTest.java
@@ -0,0 +1,14 @@
+/* 
+ * classe que demonstra o cabeçalho for sem 
+ * inicialização e expressão de iteração 
+ */
+
+class ForHeaderTest {
+
+	public static void main(String[] args) {
+
+		for (System.out.println("Ok"); true; System.out.println("Ok2"));
+
+	}
+
+} // fim da classe ForHeaderTest
