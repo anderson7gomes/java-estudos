a43d7b47925dba770bd0761cc6d7dc314172d353
anderson7gomes <gomes.andrade1@gmail.com>
2015-05-10 13:44:19 -0300

    Impossível definir um target type sem que seja functional interface

--- source/LambdaDemo.java
+++ source/LambdaDemo.java
@@ -7,11 +7,23 @@ interface MyNumber {
 	double getValue();
 } // fim da interface funcional MyNumber
 
+interface MyNumber2 {
+	
+	double getValue();
+	double getOtherValue(int number);
+	
+} // fim da interface MyNumber2
+
 class LambdaDemo {
 
 	public static void main(String[] args) {
 	
 		MyNumber number;
+		
+		/*
+		MyNumber2 number2 = (n) -> 3.14; error: MyNumber2 is not 
+										 a functional interface
+		*/								 
 		
 		number = () -> 123.45;
 		System.out.println("number.getValue(): () -> 123.45: " 
