a97882762541d4e1cca079e301d13726d2b02cf0
Anderson Gomes <gomes.andrade1@gmail.com>
2015-09-26 15:20:52 -0300

    Demonstrando a utilização do método clone()

---
+++ CloneMethodDemo.java
@@ -0,0 +1,49 @@
+class CloneMethodDemo {
+	
+	public static void main(String[] args) {
+		
+		Foo f1 = new Foo("Anderson");
+		Foo f2;
+
+		System.out.println("f1's var: " + f1.getVar());
+
+		try {
+
+			f2 = (Foo) f1.clone();	
+			System.out.println("f2's var: " + f2.getVar());
+
+		} catch (CloneNotSupportedException e) {
+
+			System.err.println("Foo not suports clone operation");
+			System.exit(1);
+
+		}
+
+	} // end main method
+
+} // end CloneMethodDemo class
+
+class Foo implements Cloneable {
+	
+	private String var;
+
+	Foo(String var) {
+		
+		this.var = var;
+
+	} // end Foo constructor -- String
+
+	String getVar() {
+		
+		return var;
+
+	} // end getVar method
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		
+		return super.clone();
+
+	} // end clone method
+
+} // end Foo class
