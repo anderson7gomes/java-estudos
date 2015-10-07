bbfa70c2a7421a2109b76865a8f900425fc812cc
anderson7gomes <gomes.andrade1@gmail.com>
2015-10-07 18:55:40 -0300

    Demonstrando a coleção LinkedHashSet

---
+++ LinkedHashSetMethodsDemo.java
@@ -0,0 +1,19 @@
+import java.util.LinkedHashSet;
+
+class LinkedHashSetMethodsDemo {
+
+	public static void main(String[] args) {
+	
+		LinkedHashSet<String> linkedHashSet = new LinkedHashSet<>();
+		
+		linkedHashSet.add("One");
+		linkedHashSet.add("Two");
+		linkedHashSet.add("Three");
+		linkedHashSet.add("Four");
+		linkedHashSet.add("Eight");
+		
+		System.out.println(linkedHashSet);
+	
+	} // end main method
+
+} // end LinkedHashSetMethodsDemo class
