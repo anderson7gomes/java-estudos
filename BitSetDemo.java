fd2b4e5155164bef96f5566ef7dd019fd06ef1b8
anderson7gomes <gomes.andrade1@gmail.com>
2015-10-22 20:48:31 -0300

    Demonstrando a classe BitSet

---
+++ BitSetDemo.java
@@ -0,0 +1,27 @@
+import java.util.BitSet;
+
+class BitSetDemo {
+
+	public static void main(String[] args) {
+	
+		BitSet bitSet1 = new BitSet();
+		
+		bitSet1.set(0);
+		bitSet1.set(1);
+		bitSet1.set(2);
+		bitSet1.clear(2);
+		bitSet1.set(5);
+		
+		BitSet bitSet2 = new BitSet();
+		
+		bitSet2.clear(0, 5);
+		
+		bitSet1.and(bitSet2);
+		
+		System.out.println(bitSet1);
+	
+		System.out.println(bitSet2);
+	
+	} // end main method
+
+} // end BitSetDemo class
