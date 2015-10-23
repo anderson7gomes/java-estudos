c982e9ed6f00a69fb67dbe11175d0652694bf59b
Anderson Gomes <gomes.andrade1@gmail.com>
2015-10-23 16:28:25 -0300

    Demonstrando a classe Date

---
+++ DateMethodsDemo.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+class DateMethodsDemo {
+
+	public static void main(String[] args) {
+	
+		Date date = new Date();
+		
+		System.out.println(date);
+		
+		long time = date.getTime();
+		
+		System.out.println(time);
+	
+	} // end main method
+
+} // end DateMethodsDemo class
