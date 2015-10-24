a513a359458dc060e6ca9e064dd54e2131232bef
Anderson Gomes <gomes.andrade1@gmail.com>
2015-10-24 17:53:41 -0300

    Demonstrando a classe GregorianCalendar

---
+++ GregorianCalendarMethodsDemo.java
@@ -0,0 +1,35 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+class GregorianCalendarMethodsDemo {
+	
+	public static void main(String[] args) {
+		
+		GregorianCalendar gregCalendar = 
+				new GregorianCalendar(2016, 
+						Calendar.MARCH, 30, 16, 30, 12);
+
+		System.out.println(gregCalendar.get(Calendar.YEAR));
+		System.out.println(gregCalendar.get(Calendar.MONTH));
+		System.out.println(gregCalendar.get(Calendar.DAY_OF_MONTH));
+		System.out.println(gregCalendar.get(Calendar.HOUR));
+		System.out.println(gregCalendar.get(Calendar.MINUTE));
+		System.out.println(gregCalendar.get(Calendar.SECOND));
+		
+		System.out.println();
+
+		gregCalendar.roll(Calendar.MONTH, 12);
+		
+		// cai na mesma data
+
+		System.out.println(gregCalendar.get(Calendar.YEAR));
+		System.out.println(gregCalendar.get(Calendar.MONTH));
+		System.out.println(gregCalendar.get(Calendar.DAY_OF_MONTH));
+		System.out.println(gregCalendar.get(Calendar.HOUR));
+		System.out.println(gregCalendar.get(Calendar.MINUTE));
+		System.out.println(gregCalendar.get(Calendar.SECOND));
+						
+
+	} // end main method
+	
+} // end GregorianCalendarMethodsDemo class
