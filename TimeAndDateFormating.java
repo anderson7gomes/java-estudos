4256fcf5b32f674f75bbe70accac968b3aceba2b
Anderson Gomes <gomes.andrade1@gmail.com>
2015-10-28 17:06:59 -0300

    Formatando datas e largura mínima no especificador de formato

---
+++ TimeAndDateFormating.java
@@ -0,0 +1,50 @@
+import java.util.Calendar;
+import java.util.Formatter;
+import java.util.GregorianCalendar;
+
+class TimeAndDateFormating {
+	
+	public static void main(String[] args) {
+		
+		Formatter formatter = new Formatter();
+
+		Calendar cal = new GregorianCalendar(2016, Calendar.AUGUST, 29);
+
+		formatter.format("%tD", cal);
+
+		System.out.println(formatter);
+		formatter.close();
+
+		formatter = new Formatter();
+
+		formatter.format("%tc", cal);
+
+		System.out.println(formatter);
+		formatter.close();
+
+		formatter = new Formatter();
+
+		for (int i = 1; i <= 10; i++) {
+			
+			formatter.format("%2s%5s%5s\n",
+					i, (int) Math.pow(i, 2), 
+					(int) Math.pow(i, 3));
+
+		}
+
+		System.out.println(formatter);
+
+		formatter.close();
+
+		formatter = new Formatter();
+
+		formatter.format("%4s\n%.3s", 
+				"Anderson", "Anderson");
+
+		System.out.println(formatter);
+		
+		formatter.close();		
+
+	} // end main method
+
+} // ebd TimeAndDateFormating class
